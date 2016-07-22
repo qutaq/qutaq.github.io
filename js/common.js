ae8478fdbc32cb298326015aafcb8641a7432a74
qutaq <maddy38rus@gmail.com>
2016-07-22 19:54:11 +0300

    vv

--- js/common.js
+++ js/common.js
@@ -1,3 +1,21 @@
 $(function(){
-  
+  $('.owl-carousel').owlCarousel({
+      loop:true,
+      margin:10,
+      nav:false,
+      dotsEach:true,
+      mouseDrag:false,
+      responsive:{
+          0:{
+              items:1
+          },
+          600:{
+              items:1
+          },
+          1000:{
+              items:1
+          }
+      }
+  })
+
 });
